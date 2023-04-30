90e9be1828b2a7474b1cf47358eb9a6784554123
mah547 <mah547@drexel.edu>
2023-04-29 22:58:01 -0400

    added error handling for when redo and undo commands stack are empty

--- Command_Pattern/commandHistory.py
+++ Command_Pattern/commandHistory.py
@@ -7,14 +7,18 @@ class CommandHistory:
 
     def pushCommandToUndo(self,command):
         self.__undoCommands.put(command)
-        self.__redoCommands.empty()
+        self.__redoCommands = LifoQueue(maxsize=0)
 
     def undo(self):
+        if self.__undoCommands.empty():
+            return
         command = self.__undoCommands.get()
         command.unexecute()
         self.__redoCommands.put(command)
 
     def redo(self):
+        if self.__redoCommands.empty():
+            return
         command = self.__redoCommands.get()
         command.execute()
         self.__undoCommands.put(command)
