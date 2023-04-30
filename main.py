12e1622e47c8c54fcf1ea27a0633111cb5dec586
chrisphamm521 <chris.pham2003@rocketmail.com>
2023-04-29 23:10:18 -0400

    Implemented Bressenham's Algorithm

--- main.py
+++ main.py
@@ -39,6 +39,20 @@ def get_row_col_from_pos(position):
     return row, col
 
 
+def plotline(row1, row2, col1, col2):
+    drow = abs(row1 - row2)
+    dcol = abs(col1 - col2)
+
+    col = col1
+    e = 0
+    for row in range(row1, row2, -1):
+        grid[row][col] = drawing_color
+        e += dcol
+        if (2 * e) > drow:
+            col += 1
+            e -= drow
+
+
 run = True
 clock = pygame.time.Clock()
 grid = init_grid(ROWS, COLS, BG_COLOR)
@@ -49,6 +63,30 @@ while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            initial = pygame.mouse.get_pos()
+
+        if event.type == pygame.MOUSEBUTTONUP:
+            final = pygame.mouse.get_pos()
+            row1, col1 = get_row_col_from_pos(initial)
+            row2, col2 = get_row_col_from_pos(final)
+
+            '''
+            if col2 < col1:
+                col1, col2 = col2, col1
+                row1, row2 = row2, row1
+            '''
+
+            if row2 > row1 and col2 > col1:
+                # Call plotline function with the original row and column values
+                plotline(row1, row2, col1, col2)
+            else:
+                # Swap the starting and ending points to make sure the line is drawn correctly
+                plotline(row2, row1, col2, col1)
+
+            plotline(row1, row2, col1, col2)
+
         '''
         if pygame.mouse.get_pressed()[0]:
             position = pygame.mouse.get_pos()
