9af472941a01f98770f7327f4f7011a5f8a165ee
mah547 <mah547@drexel.edu>
2023-04-29 22:41:38 -0400

    removed debug printing

--- utils/drawCommand.py
+++ utils/drawCommand.py
@@ -13,6 +13,5 @@ class DrawCommand(Command):
             self.__grid[pos[0]][pos[1]] = self.__drawingColor
 
     def unexecute(self):
-        print(self.__drawCache)
         for pos in self.__drawCache:
             self.__grid[pos[0]][pos[1]] = settings.WHITE
